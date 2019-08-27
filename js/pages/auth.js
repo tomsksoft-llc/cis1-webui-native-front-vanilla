e205c2066f54476cad0b840f5e67d2befc99f425
AnastasiyaPo <An98poter1>
2019-08-27 16:11:32 +0700

    correction of comments

--- js/pages/auth.js
+++ js/pages/auth.js
@@ -1,63 +1,54 @@
 /**
  * Authenticate
  *
- * @param {object} _auth     - main node
- * @param {string} _token    - user account key
- * @param {string} _username - username from cookie or input
- * @param {string} _pass     - password from input
- * @param {boolean} logged   - user logged in
+ * @param {boolean} logged - user logged in
+ *
  * Methods:
- *     init            - variable initialization
- *     signInByLogPass - sign in by login and password
- *     signOut         - log out from account
- *     onmessage       - behavior on server response
- *         @param {object} message     -  text of response
- *         for authenticate by login/pass/token:
- *             @param {string} event - success of action
- *                                  may be: 'auth.success' - success
- *                                          'auth.error.wrong_credentials' - data error
- *             @param {object} data      - message (for success)
- *                 @param {string} group - request initiator
- *                 @param {string} token - token of user
+ *  init            - variable initialization
+ *  signInByLogPass - sign in by login and password
+ *  signOut         - log out from account
+ *
+ *  onmessage       - behavior on response from server
+ *  @param {object} message - text of response text of response from server(
+ *      @param {string} event - success of action
+ *                               Variant 'auth.success' (success sign in) ||
+ *                               'auth.error.wrong_credentials' (data error) ||
+ *                               'auth.logout_success' (success sign out)
+ *      @param {object} data  - message (for success sign in)
+ *          @param {string} group - request initiator
+ *          @param {string} token - token of user
  */
 
 var Auth = {
 
-    _auth: null
+    _elements: {
+        auth: null
+        , username: null
+        , pass: null
+        , remember: null
+    }
     , _token: null
-    , _username: null
-    , _pass: null
     , logged: false
 
-    ,init: function () {
+    , init: function () {
 
-        this._auth = Selector.id('auth');
+        this._elements.auth = Selector.id('auth');
+        this._elements.username = Selector.id('auth-username');
+        this._elements.pass = Selector.id('auth-password');
+        this._elements.remember = Selector.id('auth-remember-me');
 
-        if (Cookie.get('cis_token')) {
-
-            Socket.send({
-                event: 'auth.token'
-                , transactionId: (new Date()).getTime()
-                , data: {
-                    token: decodeURIComponent(Cookie.get('cis_token'))
-                }
-            });
-
-            this._username = decodeURIComponent(Cookie.get('cis_username'));
-            this._pass = '';
-
-            this._auth.className = 'auth-sign-out';
-        } else {
-            this._auth.className = 'auth-sign-in';
-        }
+        Socket.send({
+            event: 'auth.token'
+            , transactionId: (new Date()).getTime()
+            , data: {
+                token: decodeURIComponent(Cookie.get('cis_token'))
+            }
+        });
     }
 
     , signInByLogPass: function() {
 
-        this._username = Selector.id('auth-username').value;
-        this._pass = Selector.id('auth-password').value;
-
-        if ( ! this._username) {
+        if ( ! this._elements.username.value) {
             Toast.open({
                 type: 'info'
                 , text: 'Please enter your login'
@@ -65,7 +56,7 @@ var Auth = {
             });
             return;
         }
-        if ( ! this._pass) {
+        if ( ! this._elements.pass.value) {
             Toast.open({
                 type: 'info'
                 , text: 'Please enter your password'
@@ -78,13 +69,14 @@ var Auth = {
             event: 'auth.login_pass'
             , transactionId: (new Date()).getTime()
             , data: {
-                username: this._username
-                , pass: this._pass
+                username: this._elements.username.value
+                , pass: this._elements.pass.value
             }
         });
     }
 
     , signOut: function() {
+
         Socket.send({
             event: 'auth.logout'
             , transactionId: (new Date()).getTime()
@@ -101,44 +93,45 @@ var Auth = {
             this._token = message.data.token;
             this.logged = true;
 
-            this._auth.className = 'auth-sign-out';
-
-            if (Selector.id('auth-remember-me').checked) {
+            if (this._elements.remember.checked && this._elements.username.value) {
                 Cookie.set('cis_token', encodeURIComponent(this._token));
-                Cookie.set('cis_username', encodeURIComponent(this._username));
-            } else {
-                Cookie.delete('cis_token');
-                Cookie.delete('cis_username');
+                Cookie.set('cis_username ', encodeURIComponent(this._elements.username.value));
             }
 
-            Selector.query('#auth-sign-out > span').innerHTML = this._username;
+            var username = this._elements.username.value || decodeURIComponent(Cookie.get('cis_username'));
+            Selector.query('#auth-sign-out > span').textContent += ' ' + username;
             Toast.open({
                 type: 'success'
-                , text: 'authentication was successful. You are ' + this._username
+                , text: 'authentication was successful. You are ' + username
                 , delay: 2
             });
 
+            this._elements.auth.className = 'sign-out';
+
         } else if (message.event === 'auth.error.wrong_credentials') {
 
-            Toast.open({
-                type: 'error'
-                , text: 'wrong login or password'
-                , button_close: true
-            });
+            if (this._elements.username.value) {
+                Toast.open({
+                    type: 'error'
+                    , text: 'wrong login or password'
+                    , button_close: true
+                });
+            }
+
+            this._elements.auth.className = 'sign-in';
 
         } else if (message.event === 'auth.logout_success') {
 
-            this._auth.className = 'auth-sign-in';
-
             this._token = null;
-            this._username = null;
-            this._pass = null;
             this.logged = false;
 
             Cookie.delete('cis_token');
             Cookie.delete('cis_username');
 
+            this._elements.auth.className = 'sign-in';
+
         } else {
+
             Toast.open({
                 type: 'error'
                 , text: 'Unexpected message was found' + message
@@ -147,7 +140,3 @@ var Auth = {
         }
     }
 };
-
-addEvent(window, 'load', function() {
-    Auth.init();
-});
